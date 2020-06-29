99f9663e47c49dfe51f091f0b15369b8b6bf73a5
stephenzshu <stephenzshu@gmail.com>
2020-06-29 18:30:08 -0500

    Comments and clarity.

--- scrape_syllabi.py
+++ scrape_syllabi.py
@@ -6,34 +6,37 @@ import time
 DELAY = 0
 counter = 1
 
-#Run this script in the directory that you would like to dump syllabi in.
-#Downloads all syllabi iterating through all semesters from the official UT Syllabi Directory.
+#Downloads all syllabi iterating through all semesters from the official UT Syllabi Directory(3000+ files).
 #Prevents repeat files and can recheck quickly.
 #Takes 30 min - 1 hour to run the first time.
 
 if not path.exists('syllabi'):
 	print('Create the syllabi directory or change the path of the output files')
 
+#Parses all semesters available from the options.
 base_url = "https://utdirect.utexas.edu/apps/student/coursedocs/nlogon/"
 query = "?semester=&department=&course_number=&course_title=&unique=&instructor_first=&instructor_last=&course_type=In%20Residence&search=Search"
 URL = base_url + query
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
-subject_options = soup.select('select[name=semester] > option')
+semesters = soup.select('select[name=semester] > option')
 
-for semester in subject_options[1:]:
+#Iterate through courses for all semesters.
+for semester in semesters[1:]:
 	s = semester['value']
 	query = "?semester=" + s + "&department=&course_number=&course_title=&unique=&instructor_first=&instructor_last=&course_type=In%20Residence&search=Search"
 	URL = base_url + query
 	page = requests.get(URL)
-
 	soup = BeautifulSoup(page.content, "html.parser")
 	
+	#Find all links on the page.
 	for links in soup.find_all('a', href=True):
+		#For this website, links to syllabi are named "View".
 		if "View" in links.contents[0]:
 			full_link = base_url + links['href']
 			print('Downloading syllabus #%d: %s' % (counter,full_link))
 			name = 'syllabi/syllabus_' + links['href'].split('/')[1] + '.pdf'
+			#Skip if the file has been downloaded already, otherwise, download as pdf.
 			if path.exists(name):
 				print('File already exists')
 			else:
